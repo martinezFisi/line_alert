0276a34e39ee08338e50ea3f48176bff92c1f459
Antony Martinez <antonymartinez12@gmail.com>
2025-12-03 22:32:07 -0500

    Se agregan las apps al settings del proyecto

--- line_alert_project/settings.py
+++ line_alert_project/settings.py
@@ -31,6 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'web_app',
+    'scrapping_app',
+    'notifications_app',
+
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
