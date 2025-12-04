504108c250a4cc1d449b510fe73d2d175a608a77
Antony Martinez <antonymartinez12@gmail.com>
2025-12-03 22:45:30 -0500

    Se crean los modelos, se crea el archivo de migracion y se registran los modelos en el site de administracion

--- web_app/admin.py
+++ web_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Line, QueryHistory, Notification
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Line)
+admin.site.register(QueryHistory)
+admin.site.register(Notification)
--- web_app/models.py
+++ web_app/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class User(AbstractUser):
+    document_type = models.CharField(max_length=100),
+    document_number = models.CharField(max_length=100),
+    verification_phone_number = models.CharField(max_length=15),
+    is_active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return f"{self.type} - {self.number} - {self.email} - {self.phone_number} - {'Active' if self.is_active else 'Inactive'}"
+
+class Line(models.Model):
+    phone_number = models.CharField(max_length=15),
+    lineType = models.CharField(max_length=50),
+    operator = models.CharField(max_length=50),
+    is_new_line = models.BooleanField(default=False)
+    monitored_document = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lines')
+
+class QueryHistory(models.Model):
+    lines_found = models.IntegerField(),
+    new_lines_found = models.IntegerField(),
+    monitored_document = models.ForeignKey(User, on_delete=models.CASCADE, related_name='query_histories')
+
+class Notification(models.Model):
+    email = models.EmailField(),
+    message = models.TextField(),
+    monitored_document = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
